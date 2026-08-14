7220f15934f8546e672f896e98af4be6238bd3df
andersonfranciscoig@gmail.com <anddev@andecode.localdomain>
2026-08-14 15:00:23 +0100

    feat: update

--- src/modules/platform-beta/platform-beta.module.ts
+++ src/modules/platform-beta/platform-beta.module.ts
@@ -1,5 +1,4 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { Module, forwardRef } from '@nestjs/common';
 import { IdentityModule } from '../identity/identity.module';
 import { MailModule } from '../mail/mail.module';
 import { PlatformBetaService } from './application/platform-beta.service';
@@ -7,7 +6,7 @@ import { PlatformBetaAdminController } from './infrastructure/http/controllers/p
 import { PlatformBetaPublicController } from './infrastructure/http/controllers/platform-beta-public.controller';
 
 @Module({
-  imports: [MailModule, IdentityModule],
+  imports: [MailModule, forwardRef(() => IdentityModule)],
   controllers: [PlatformBetaPublicController, PlatformBetaAdminController],
   providers: [PlatformBetaService],
   exports: [PlatformBetaService],
